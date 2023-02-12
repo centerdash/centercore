8010845cc524cfd9d9592e126c2ea3285cd64864
Kirill <87936030+MisterKirill@users.noreply.github.com>
2023-02-12 15:06:48 +0500

    ok

--- src/features/misc/getAccountURL.ts
+++ src/features/misc/getAccountURL.ts
@@ -8,9 +8,6 @@ type Body = {
 export default async function handler(req: FastifyRequest<{ Body: Body }>, rep: FastifyReply) {
     if(!req.body.type) return -1
 
-    //TODO: make it better or not :/
-    let host = `http://${process.env.SERVER_DOMAIN}:${process.env.SERVER_PORT}${process.env.SERVER_BASE_PATH}`
-
-    Logger.event_get(`Host fetched (${host})`)
-    return host
+    Logger.event_get('Host fetched')
+    return process.env.SERVER_OUT_HOST
 }
